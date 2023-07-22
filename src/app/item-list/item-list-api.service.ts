72a0c21a8ed14c9e2afc4d50b81d02b01227c598
yasith saparamadu <yasith@azbow.com>
2023-07-22 23:24:02 +0530

    get hours and UI update

--- src/app/item-list/item-list-api.service.ts
+++ src/app/item-list/item-list-api.service.ts
@@ -11,7 +11,7 @@ export class ApiService {
 
   constructor(private http: HttpClient) {}
 
-  getCleanersText(): Observable<any> {
+  getDetails(): Observable<any> {
     return this.http.get<any>(this.apiUrl); 
   }
 }
--- src/app/item-list/item-list.component.ts
+++ src/app/item-list/item-list.component.ts
@@ -8,21 +8,38 @@ import { ApiService } from "./item-list-api.service";
 })
 export class ItemListComponent implements OnInit {
   cleanersText: string = "";
+  hoursText: string = "";
   selectedImage: number | null = null;
+  imageUrls: string[] = [];
+  cardNumbers: string[] = [];
   imagePath1: string = "assets/1person.png";
   imagePath2: string = "assets/2people.png";
   imagePath3: string = "assets/3people.png";
+  hoursData: any[] = []; // Updated data structure to hold hours for each card
 
   constructor(public apiService: ApiService) {}
 
   ngOnInit() {
-    this.getCleanersText();
+    this.getDetails();
   }
 
-  getCleanersText() {
-    this.apiService.getCleanersText().subscribe(
+  getDetails() {
+    this.apiService.getDetails().subscribe(
       (response) => {
-        this.cleanersText = response.data.title;
+        if (response && response.data && response.data.title) {
+          this.cleanersText = response.data.title;
+          this.imageUrls = response.data.items.map(
+            (item: { image: any }) => item.image
+          );
+          this.cardNumbers = response.data.items.map(
+            (item: { displayText: any }) => item.displayText
+          );
+          this.hoursText = response.data.itemTitle;
+          this.hoursData = response.data.items.map((item: any) => item.items); // Store the hours for each card
+        } else {
+          console.error("Invalid API response format: title not found.");
+        }
+        console.log("API Response Data:", response);
       },
       (error) => {
         console.error("Error fetching text:", error);
@@ -32,5 +49,14 @@ export class ItemListComponent implements OnInit {
 
   selectImage(imageNumber: number) {
     this.selectedImage = imageNumber;
+  }
+
+  // Function to get the selected card's hours
+   getSelectedCardHours(index: number) {
+    if (this.hoursData[index - 1]) {
+      const selectedCardItems = this.hoursData[index - 1];
+      return selectedCardItems.map((item: any) => item.itemName);
+    }
+    return [];
   }
 }
